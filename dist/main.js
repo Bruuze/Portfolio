dd0f8c4c84df01b1756d5d52fba48a6bb00c073b
Bruuze <aidan.murphy.personal@gmail.com>
2020-09-01 05:00:12 +0100

    js fix

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,2 @@
-setTimeout(function(){ $("body > #__nuxt").css("opacity", "1"); }, 500);
+setTimeout(function(){ $("body > #__nuxt").css("opacity", "1"); }, 2000);
 setTimeout(function(){ $("body").css("overflow-y", "auto"); }, 3500);
