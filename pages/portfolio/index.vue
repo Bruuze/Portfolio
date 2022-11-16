a37255cb76276f1e30fac4a1fabc1c61a8bd9493
Bruuze <aidan.murphy.personal@gmail.com>
2022-11-16 21:36:43 +0000

    Update index.vue

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -36,11 +36,16 @@ export default {
           // hid is used as unique identifier. Do not use `vmid` for it as it will not work
           {hid: 'og:title',name: 'og:title', content: "Aidan Murphy - Work - Designs for Streamers"},
           {hid: 'og:description',name: 'og:description', content: "Aidan Murphy - Work - Web, Motion & Graphic Design all in one place, made for Streamers!"},
-          {hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image'},
+          {hid: 'keywords', name: 'keywords', content: 'stream, design, twitch, graphics, youtube, panels, package, scenes, screens, offline, offline image, banner, content, creation, influncer, facebook, streaming, quality, designer, code, website, motion, animation, animated, aidan, murphy, VBI, Nerd, or, die, Nerdordie, visuals, by, impulse, youtuber, irish, ireland, english'},
+
           {hid: 'twitter:title', name: 'twitter:title', content: 'Aidan Murphy - Work - Designs for Streamers'},
           {hid: 'twitter:description', name: 'twitter:description', content: 'Aidan Murphy - Work - Web, Motion & Graphic Design all in one place, made for Streamers!'},
-          // image must be an absolute path
+          {hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image'},
           {hid: 'twitter:image', name: 'twitter:image', content: this.rootPath + this.pageImg},
+          {hid: 'twitter:image:alt' , name: 'twitter:image:alt', content: 'Aidan Murphy Portflio: Designs for Streamers & Content Creators'},
+          {hid: 'twitter:site', name: 'twitter:site', content: this.twitterUsername},
+          {hid: 'twitter:creator', name: 'twitter:creator', content: this.twitterUsername},
+
           // Facebook OpenGraph
           {property: 'og:title', content: 'Aidan Murphy - Work - Designs for Streamers'},
           {property: 'og:site_name', content: 'Aidan Murphy - Portfolio'},
