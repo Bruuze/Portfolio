2ba96b011c89c7cf69933dd51adb23b36c5c0808
Bruuze <aidan.murphy.personal@gmail.com>
2022-11-17 01:33:09 +0000

    tests

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,6 +13,12 @@
         <li class="nav__li nav__li--desktop-only">
           <nuxt-link to="/blog/" class="nav__link-text" title="Blog"> Blog </nuxt-link>
         </li>
+        <li class="nav__li nav__li--desktop-only">
+          <nuxt-link to="/test2/" class="nav__link-text" title="Test1"> Test1 </nuxt-link>
+        </li>
+        <li class="nav__li nav__li--desktop-only">
+          <nuxt-link to="/test2/" class="nav__link-text" title="Test2"> Test2 </nuxt-link>
+        </li>
       </ul>
       <ul class="nav__ul sematic-only">
         <li class="nav__li nav__li--mobile-only">
